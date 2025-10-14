e92df69a81d78628bb589d657fa318e45042c089
pr401n <pr401n@gmail.com>
2025-10-14 20:06:31 +0300

    separate model from training

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 384
 n_head = 6
